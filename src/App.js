12f7c5d32ee44765a4be5e0b85edb48fea88286b
Katya383 <82849906+Katya383@users.noreply.github.com>
2021-08-13 16:14:50 -0400

    Added Netlify link

--- src/App.js
+++ src/App.js
@@ -17,10 +17,16 @@ export default function App() {
           <Dictionary defaultKeyword="dawn" />
         </main>
         <footer className="App-footer">
-          <small>Coded by Katia Barnave and </small>
+          <small>This project was coded by Katia Barnave and </small>
           <a href="https://github.com/Katya383/dictionary-project">
             {" "}
-            open-sourced
+            open-sourced on Github
+          </a>
+          {""}
+          and
+          <a href="https://brave-gates-d5766c.netlify.app/">
+            {""}
+            and hosted on Netlify.
           </a>
         </footer>
       </div>
